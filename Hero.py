845a45dfd48562879fab23fc02a4c0f91abce408
Taras Ignatiy <emporio86@gmail.com>
2017-07-12 23:26:52 +0300

    added map coordinates

--- Hero.py
+++ Hero.py
@@ -10,22 +10,22 @@ class Hero:
     def __str__(self):
         string = "Your name: " + self.name + "\n"
         string += "You have " + str(self.health) + " hearts\n"
-        string += "Your armor: " + str(self.armor)+ "\n"
+        string += "Your armor: " + str(self.armor) + "\n"
         string += "And your location X: " + str(self.locationX) + " Y: " + str(self.locationY) + "\n"
         return string
 
-    @staticmethod
-    def move_forward():
+    def move_forward(self):
+        self.locationX += 1
         return "You move forward"
 
-    @staticmethod
-    def move_back():
+    def move_back(self):
+        self.locationX -= 1
         return "You move back"
 
-    @staticmethod
-    def move_right():
+    def move_right(self):
+        self.locationY += 1
         return "You move right"
 
-    @staticmethod
-    def move_left():
+    def move_left(self):
+        self.locationY -= 1
         return "You move left"
