019ae994aa5c6788225d6ed12b01f0e194a2a903
Taras Ignatiy <emporio86@gmail.com>
2017-07-13 21:44:06 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TheGame',
+    version='0.0.0.1a',
+    packages=[''],
+    url='https://github.com/tis86/theGame',
+    license='',
+    author='Taras Ihnatii',
+    author_email='emporio86@gmail.com',
+    description='simple game for studying Python'
+)
