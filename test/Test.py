59f1aede3206fddca9dcfe760841187d59eb0074
Taras Ignatiy <emporio86@gmail.com>
2017-09-04 21:54:16 +0300

    added simple test

--- test/Test.py
+++ test/Test.py
@@ -1,7 +1,25 @@
 import unittest
+from Hero import Hero as Character
 
 
 class TestHeroMethod(unittest.TestCase):
+    Character.name = "NPC"
+    Character.armor = 0
+    Character.health = 10
+    Character.locationX = 0
+    Character.locationY = 0
 
-    def test_forward(self):
-        self.assertEqual(1, 1, "Don`t work")
+    def testName(self):
+        self.assertEqual(Character.name, "NPC", "don`t work!")
+
+    def testArmor(self):
+        self.assertEqual(Character.armor, 0, "Don`t ok")
+
+    def testHealth(self):
+        self.assertEqual(Character.health, 10, "Don`t ok")
+
+    def testLocationX(self):
+        self.assertEqual(Character.locationX, 0, "Don`t ok")
+
+    def testLocationY(self):
+        self.assertEqual(Character.locationY, 0, "Don`t ok")
